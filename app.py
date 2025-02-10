d51aa53818cc226b8f7bede1193aede5710c9026
ACupOfJrink <2023331582@qq.com>
2025-02-10 21:47:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ page0 = st.Page("pages/page0.py", title="队形算法")
 
 page1 = st.Page("pages/page1.py", title="定位算法")
 
-page2 = st.Page("pages/page2.py", title="算法对比")
+page2 = st.Page("pages/page2.py", title="仿真模拟")
 
 page3 = st.Page("pages/page3.py", title="避障算法")
 
